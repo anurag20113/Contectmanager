a445e469aecf96e5521d00bfddba2ad8afbf020d
anurag20113 <103733677+anurag20113@users.noreply.github.com>
2022-04-16 09:45:34 +0530

    frist commit

---
+++ src/main/java/com/project01/passwordService.java
@@ -0,0 +1,17 @@
+package com.project01;
+
+import java.beans.Encoder;
+import java.util.Base64;
+
+public class passwordService {
+       
+	public static String encode(String txt) {
+	Encoder encoder = Base64.getEncoder();
+	
+	}
+	public static void main(String[] args) {
+		
+
+	}
+
+}
